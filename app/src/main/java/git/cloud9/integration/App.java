14abb747887ac24a3991a77aca858066070ac782
EC2 Default User <ec2-user@ip-172-31-12-139.ec2.internal>
2021-04-07 04:20:42 +0000

    Initial Commit After creating new project through cloud9 IDE

---
+++ app/src/main/java/git/cloud9/integration/App.java
@@ -0,0 +1,15 @@
+package git.cloud9.integration;
+
+import java.util.*;
+import java.lang.*;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+        List<Integer> inputList = Arrays.asList(3 ,4);
+    }
+}
